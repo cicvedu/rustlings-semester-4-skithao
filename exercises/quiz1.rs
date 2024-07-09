e75a9b290d066d283a619840afc3ff75ad71afc5
skithao <zbc123581@163.com>
2024-07-09 10:59:38 +0800

    前22题提交测试

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,19 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
+    fn calculate_price_of_apples(quantity: i32) -> i32 {
+        let base_price = 2;
+        let discount_price = 1;
+    
+        if quantity > 40 {
+            quantity * discount_price
+        } else {
+            quantity * base_price
+        }
+    }
 
 // Don't modify this function!
 #[test]
@@ -31,3 +40,5 @@ fn verify_test() {
     assert_eq!(41, price3);
     assert_eq!(65, price4);
 }
+
+
